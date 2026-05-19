5ad4749bb56fe495cfaa7a96d1be81e80f09b0fd
ScarletRuelas <Zarcsextans@gmail.com>
2026-05-19 08:37:09 -0600

    CREATE: Domain - Item, IIitemRepository

---
+++ CatalogoApp.Domain/Interfaces/IIitemRepository.cs
@@ -0,0 +1,16 @@
+﻿using CatalogoApp.Domain.Models;
+
+
+
+namespace CatalogoApp.Domain.Interfaces
+{
+    public interface IItemRepository
+    {
+        List<Item> ObtenerTodos();
+        Item? ObtenerPorId(int id);
+        void Agregar(Item item);
+        void Eliminar(int id);
+    }
+}
+
+
---
+++ CatalogoApp.Domain/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace CatalogoApp.Domain.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Genero { get; set; } = string.Empty;
+        public string Consola { get; set; } = string.Empty;
+        public int Ano { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+    }
+}
+
+
