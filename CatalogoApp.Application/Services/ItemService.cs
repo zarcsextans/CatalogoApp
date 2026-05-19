b6766387b6c93814309eb816d9c831b86a9c9a0c
ScarletRuelas <Zarcsextans@gmail.com>
2026-05-19 08:42:42 -0600

    CREATE: Application - ItemService

---
+++ CatalogoApp.Application/Services/ItemService.cs
@@ -0,0 +1,73 @@
+﻿using CatalogoApp.Domain.Interfaces;
+using CatalogoApp.Domain.Models;
+
+
+
+namespace CatalogoApp.Application.Services
+{
+    public class ItemService
+    {
+        private readonly IItemRepository _repo;
+
+
+
+        // El servicio recibe el repositorio por constructor
+        // No sabe si es JSON, SQL, memoria, etc.
+        public ItemService(IItemRepository repo)
+        {
+            _repo = repo;
+        }
+
+
+
+        public List<Item> ObtenerTodos()
+        {
+            return _repo.ObtenerTodos();
+        }
+
+
+
+        public Item? ObtenerPorId(int id)
+        {
+            return _repo.ObtenerPorId(id);
+        }
+
+
+
+        public void Agregar(Item item)
+        {
+            // Aquí podrías agregar validaciones de negocio
+            // Por ejemplo: if (string.IsNullOrEmpty(item.Titulo)) throw...
+            _repo.Agregar(item);
+        }
+
+
+
+        public void Eliminar(int id)
+        {
+            _repo.Eliminar(id);
+        }
+
+
+
+        // Método útil para el filtro por categoría/género
+        public List<Item> ObtenerPorGenero(string genero)
+        {
+            return _repo.ObtenerTodos()
+                        .Where(i => i.Genero == genero)
+                        .ToList();
+        }
+
+
+
+        public List<string> ObtenerGeneros()
+        {
+            return _repo.ObtenerTodos()
+                        .Select(i => i.Genero)
+                        .Distinct()
+                        .ToList();
+        }
+    }
+}
+
+
