7c7c1c7ed3ad24751bd711f3995945fe4c9c239b
ScarletRuelas <Zarcsextans@gmail.com>
2026-05-20 10:07:02 -0600

    CREATE: Catalogos cshtml

--- CatalogoApp.Presentation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CatalogoApp.Presentation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
